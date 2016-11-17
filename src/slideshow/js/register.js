8c1c460707a6ce2a560a52d54a0e85509203b1d4
Cédric EBERHARDT <design@frontools.rocks>
2016-11-17 15:01:17 +0100

    update slideshow component, now simpler, bullets independant

--- src/slideshow/js/register.js
+++ src/slideshow/js/register.js
@@ -1,5 +1,16 @@
-import SrcSet from "frontools/helpers/scrset";
-import get_brightness from "frontools/helpers/image.brightness";
+function create_custom_event ( eventName, detail ) {
+	let changeEvent = null;
+
+	try {
+		changeEvent = new CustomEvent( eventName, { detail } );
+	}
+	catch ( err ) { // IE11
+		changeEvent = document.createEvent( "CustomEvent" );
+		changeEvent.initCustomEvent( eventName, true, true, { detail } );
+	}
+
+	return changeEvent;
+}
 
 class Slideshow extends HTMLElement {
 	constructor () {
@@ -7,207 +18,17 @@ class Slideshow extends HTMLElement {
 	}
 
 	/**
-	 * Get the source of the element depending on the media queries
-	 * @see helpers/srcset
-	 * @param {HTMLElement} slide - current slide
-	 * @returns {{picture: string, video: string}} - urls of each media
-	 * @private
-	 */
-	_get_srcset ( slide ) {
-		var fragment = slide.querySelector( "template" ),
-			picture, video;
-
-		if ( fragment ) {
-			try { fragment = fragment.content.cloneNode( true ); }
-			catch ( error ) { /* IE11 doesn't support <template> */ }
-			finally {
-				const pictureElement = fragment.querySelector( "picture" ),
-					videoElement = fragment.querySelector( "video" );
-
-				picture = pictureElement ? new SrcSet( pictureElement ).src : undefined;
-				video = videoElement ? new SrcSet( videoElement ).src : undefined;
-			}
-		}
-
-		return { picture, video };
-	}
-
-	/**
-	 * Download an image
-	 * @param {string} url - url of the image
-	 * @returns {Promise} - The image as a blob
-	 * @private
-	 */
-	_load_image ( url ) {
-		return new Promise( function ( resolve, reject ) {
-			var request = new XMLHttpRequest();
-
-			request.open( "GET", url );
-			request.responseType = "blob";
-			request.addEventListener( "load", function () {
-				if ( request.status === 200 ) {
-					resolve( request.response );
-				}
-				else {
-					reject( Error( `Image didn't load successfully; error code:${ request.statusText }` ) );
-				}
-			} );
-			request.addEventListener( "error", function () {
-				reject( Error( "There was a network error." ) );
-			} );
-			request.send();
-		} );
-	}
-
-	/**
-	 * Render the DOM. If a video is defined, it plays from now.
-	 * @param {Function} [callback = null] Callback function
-	 * @returns {void}
-	 */
-	render ( callback = null ) {
-		var self = this,
-			fragment = document.createDocumentFragment();
-
-		/**
-		 * Check if all slides have been done and do last config
-		 * @this Slideshow
-		 * @returns {void}
-		 */
-		function check_status () {
-			if ( this.loaded >= this.count ) {
-				if ( this.bulletsContainer ) {
-					this.bulletsContainer.firstElementChild.remove();
-					this.bullets = this.bulletsContainer.children;
-
-					[ ...this.bullets ].forEach( ( bullet, index ) => bullet.dataset.goto = index );
-				}
-
-				this.app.appendChild( fragment );
-				this.backgroundSlides = this.app.querySelectorAll( ".slide" );
-				this.slides = this.$element.querySelectorAll( ".slide" );
-				this.run();
-
-				if ( callback && typeof callback === "function" ) {
-					callback();
-				}
-			}
-		}
-
-		if ( !this.app ) {
-			this.app = document.createElement( "div" );
-			this.app.className = "background";
-			this.$element.appendChild( this.app );
-		}
-
-		[ ...this.slides ].forEach( ( slide, index ) => {
-			const tree = document.createElement( "div" ),
-				{ picture, video } = this._get_srcset( slide ),
-				bullet = this.bulletElement ? this.bulletElement.cloneNode( true ) : null;
-
-			if ( typeof video !== "undefined" ) {
-				const sourceElement = document.createElement( "source" ),
-					videoElement = document.createElement( "video" );
-
-				sourceElement.setAttribute( "src", video );
-				sourceElement.setAttribute( "type", "video/mp4" );
-
-				videoElement.setAttribute( "loop", "true" );
-				videoElement.setAttribute( "autoplay", "true" );
-				videoElement.setAttribute( "muted", "true" );
-				videoElement.setAttribute( "webkit-playsinline", "true" );
-				videoElement.setAttribute( "poster", video );
-
-				if ( slide.dataset.position ) {
-					videoElement.style.objectPosition = slide.dataset.position;
-				}
-
-				videoElement.appendChild( sourceElement );
-				tree.appendChild( videoElement );
-			}
-
-			if ( bullet ) {
-				bullet.removeAttribute( "hidden" );
-				bullet.addEventListener( "click", this._goto_ev.bind( this ) );
-				this.bulletsContainer.appendChild( bullet );
-			}
-
-			tree.className = "slide";
-
-			if ( slide.dataset.position ) {
-				tree.style.backgroundPosition = slide.dataset.position;
-			}
-
-
-			if ( index === 0 ) { this.app.appendChild( tree ); }
-			else { fragment.appendChild( tree ); }
-
-			this._load_image( picture )
-				.then( response => {
-					tree.style.backgroundImage = `url(${ URL.createObjectURL( response ) })`;
-					self.loaded++;
-					check_status.apply( this );
-
-					try {
-						const img = document.createElement( "img" );
-
-						img.onload = () => {
-							const brightness = get_brightness( img );
-
-							slide.dataset.brightness = ( brightness > 128 ) ? "light" : "dark";
-							tree.dataset.brightness = String( brightness );
-						};
-						img.src = URL.createObjectURL( response );
-					}
-					catch ( e ) {
-						tree.dataset.brightness = "-1";
-					}
-				} )
-				.catch( err => {
-					this.$element.removeChild( slide );
-					if ( index === 0 ) { this.app.removeChild( tree ); }
-					else { fragment.removeChild( tree ); }
-					if ( bullet ) { this.bulletsContainer.removeChild( bullet ); }
-
-					self.count--;
-					check_status.apply( this );
-				} );
-		} );
-	}
-
-	/**
 	 * Add, remove CSS classes to the slides. Content and Background.
 	 * @returns {void}
 	 * @private
 	 */
-	_show () {
-		const CURRENT_SLIDE = this.slides[ this.current ],
-			CURRENT_BACKGROUND_SLIDE = this.backgroundSlides[ this.current ];
-		let changeEvent;
-
-		this.occurrence++;
-
-		try {
-			changeEvent = new CustomEvent(
-				"change",
-				{
-					detail: {
-						id: this.current,
-						brightness: CURRENT_SLIDE.dataset.brightness,
-						occurrence: this.occurrence
-					}
-				}
-			);
-		}
-		catch ( err ) {
-			changeEvent = document.createEvent( "CustomEvent" );
-			changeEvent.initCustomEvent( "change", true, true, {
-				detail: {
-					id: this.current,
-					brightness: CURRENT_SLIDE.dataset.brightness,
-					occurrence: this.occurrence
-				}
+	_run () {
+		const CURRENT_SLIDE = this.$foregroundSlides[ this.current ],
+			CURRENT_BACKGROUND_SLIDE = this.$backgroundSlides[ this.current ],
+			CHANGE_EVENT = create_custom_event( "change", {
+				id: this.current,
+				occurrence: ++this.occurrence
 			} );
-		}
 
 		/**
 		 * Remove last slide CSS classes when transition ends
@@ -217,10 +38,10 @@ class Slideshow extends HTMLElement {
 		 * @private
 		 */
 		function transition_end ( index ) {
-			this.slides[ index ].classList.remove( "is-show" );
-			this.slides[ index ].classList.remove( "is-last" );
-			this.backgroundSlides[ index ].classList.remove( "is-show" );
-			this.backgroundSlides[ index ].classList.remove( "is-last" );
+			this.$foregroundSlides[ index ].classList.remove( "is-show" );
+			this.$foregroundSlides[ index ].classList.remove( "is-last" );
+			this.$backgroundSlides[ index ].classList.remove( "is-show" );
+			this.$backgroundSlides[ index ].classList.remove( "is-last" );
 			if ( this.bullets ) {
 				this.bullets[ index ].classList.remove( "is-show" );
 				this.bullets[ index ].classList.remove( "is-last" );
@@ -230,23 +51,18 @@ class Slideshow extends HTMLElement {
 		for ( let i = 0; i < this.count; i++ ) {
 			if ( this.last === i ) {
 				if ( this.current !== this.last ) {
-					this.slides[ i ].classList.add( "is-last" );
-					this.backgroundSlides[ i ].classList.add( "is-last" );
-					if ( this.bullets ) { this.bullets[ i ].classList.add( "is-last" ); }
+					this.$foregroundSlides[ i ].classList.add( "is-last" );
+					this.$backgroundSlides[ i ].classList.add( "is-last" );
 					this.timeoutID = setTimeout( transition_end.bind( this, i ), this.transitionDelay );
 				}
 			}
 			else {
-				this.slides[ i ].classList.remove( "is-last" );
-				this.backgroundSlides[ i ].classList.remove( "is-show" );
-				this.backgroundSlides[ i ].classList.remove( "is-last" );
-				if ( this.bullets ) {
-					this.bullets[ i ].classList.remove( "is-show" );
-					this.bullets[ i ].classList.remove( "is-last" );
-				}
+				this.$foregroundSlides[ i ].classList.remove( "is-last" );
+				this.$backgroundSlides[ i ].classList.remove( "is-show" );
+				this.$backgroundSlides[ i ].classList.remove( "is-last" );
 			}
 
-			this.slides[ i ].classList.remove( "is-show" );
+			this.$foregroundSlides[ i ].classList.remove( "is-show" );
 		}
 
 		CURRENT_SLIDE.classList.add( "is-show" );
@@ -259,12 +75,11 @@ class Slideshow extends HTMLElement {
 				CURRENT_BACKGROUND_SLIDE.classList.remove( "is-first" );
 			}, this.transitionDelay );
 		}
-		if ( this.bullets ) { this.bullets[ this.current ].classList.add( "is-show" ); }
 
 		this.last = this.current;
 		this.current = ( this.current + 1 ) % this.count;
 
-		this.$element.dispatchEvent( changeEvent );
+		this.dispatchEvent( CHANGE_EVENT );
 	}
 
 	/**
@@ -275,17 +90,9 @@ class Slideshow extends HTMLElement {
 	goto ( slideNumber ) {
 		clearInterval( this.intervalID );
 		clearTimeout( this.timeoutID );
-		this.current = slideNumber % this.count;
-		this._show();
+		this.current = slideNumber < 0 ? slideNumber % this.count + this.count : slideNumber % this.count;
+		this._run();
 	}
-
-	/**
-	 * When click on a bullet, go to this slide
-	 * @param {Event} event - Mouse Event
-	 * @returns {void}
-	 * @private
-	 */
-	_goto_ev ( event ) { this.goto( event.currentTarget.dataset.goto );	}
 
 	/**
 	 * Go to the next slide
@@ -293,42 +100,35 @@ class Slideshow extends HTMLElement {
 	 */
 	next () { this.goto( this.current + 1 ); }
 
+	/**
+	 * Go to the previous slide
+	 * @returns {void}
+	 */
+	previous () { this.goto( this.current + 1 ); }
+
 	createdCallback () {
 		this.classList.add( "magnolia-slideshow" );
 
-		this.$element = this;
-		this.app = null;
-		this.slides = this.querySelectorAll( ".slide" );
+		this.$foregroundSlides = this.querySelectorAll( ".foreground > .slide" );
+		this.$backgroundSlides = this.querySelectorAll( ".background > .slide" );
 
-		this.count = this.slides.length;
-		this.delay = Number( this.dataset.delay );
+		this.count = this.$foregroundSlides.length;
 		this.transitionDelay = Number( this.dataset.transition );
+
 		this.current = 0;
 		this.last = -1;
 		this.intervalID = 0;
 		this.timeoutID = 0;
-		this.bullets = null;
-		this.loaded = 0;
 		this.occurrence = 0;
-
-		try {
-			this.bulletsContainer = this.$element.querySelector( ".Slideshow-Bullets" );
-			this.bulletElement = this.bulletsContainer.firstElementChild;
-		}
-		catch ( error ) {
-			this.bulletsContainer = null;
-			this.bulletElement = null;
-		}
-
-		this.render();
 	}
 
-	// attachedCallback () {
-	run () {
-		setTimeout( this._show.bind( this ), 16 );
+	attachedCallback () {
+		const DELAY = Number( this.dataset.delay );
 
-		if ( this.count > 1 && this.delay > 0 ) {
-			this.intervalID = setInterval( this._show.bind( this ), this.delay );
+		setTimeout( this._run.bind( this ), 16 );
+
+		if ( this.count > 1 && DELAY > 0 ) {
+			this.intervalID = setInterval( this._run.bind( this ), DELAY );
 		}
 	}
 }
--- webapp/slideshow/templates/components/slideshow.ftl
+++ webapp/slideshow/templates/components/slideshow.ftl
@@ -1,39 +1,47 @@
 <magnolia-slideshow
-	data-delay="5000"
-	data-transition="1500">
+    data-delay="5000"
+    data-transition="1500">
 [#if content.slides?has_content]
+	<div class="foreground">
 	[#list cmsfn.children(content.slides) as slide]
-        <div class="slide">
-            <template hidden>
-				[#if slide.image?has_content]
-					[#assign image=slide.image]
-                    <picture>
-                        <source media="(max-width: 376px)"
-                                srcset="${damfn.getRendition(image, "smartphone").getLink()}, ${damfn.getRendition(image, "smartphone-2x").getLink()} 2x">
-                        <source media="(max-width: 668px)"
-                                srcset="${damfn.getRendition(image, "phablet").getLink()}, ${damfn.getRendition(image, "phablet-2x").getLink()} 2x">
-                        <source media="(max-width: 1025px)"
-                                srcset="${damfn.getRendition(image, "tablet").getLink()}, ${damfn.getRendition(image, "tablet-2x").getLink()} 2x">
-                        <source media="(max-width: 1441px)"
-                                srcset="${damfn.getRendition(image, "desktop").getLink()}, ${damfn.getRendition(image, "desktop-2x").getLink()} 2x">
-                        <source media="(max-width: 1921px)"
-                                srcset="${damfn.getRendition(image, "wide").getLink()}, ${damfn.getRendition(image, "wide-2x").getLink()} 2x">
-                        <source
-                                srcset="${damfn.getRendition(image, "extra-wide").getLink()}, ${damfn.getRendition(image, "extra-wide-2x").getLink()} 2x">
-                    </picture>
-				[/#if]
-
-				[#if slide.video?has_content]
-                    <video>
-                        <source src="${damfn.getAssetLink(slide.video)!}">
-                    </video>
-				[/#if]
-            </template>
-
+		<div class="slide">
 			[#if slide.caption?has_content]
-                <div class="caption"> ${cmsfn.decode(slide).caption!}</div>
+			<div class="caption"> ${cmsfn.decode(slide).caption!}</div>
 			[/#if]
-        </div>
+		</div>
 	[/#list]
+	</div>
+
+	<div class="background">
+	[#list cmsfn.children(content.slides) as slide]
+		<div class="slide">
+			[#if slide.video?has_content]
+			<video autoplay loop muted playsinline
+				   [#if slide.image?has_content]poster="${damfn.getRendition(slide.image, "slideshow-1024").getLink()}"[/#if]>
+				<source src="${damfn.getAssetLink(slide.video)!}" type="video/mp4">
+			</video>
+			[/#if]
+
+			[#if slide.image?has_content]
+			<picture>
+				<source media="(max-width: 376px)"
+						srcset="${damfn.getRendition(slide.image, "slideshow-375").getLink()}, ${damfn.getRendition(slide.image, "slideshow-375@2x").getLink()} 2x">
+				<source media="(max-width: 668px)"
+						srcset="${damfn.getRendition(slide.image, "slideshow-667").getLink()}, ${damfn.getRendition(slide.image, "slideshow-667@2x").getLink()} 2x">
+				<source media="(max-width: 1025px)"
+						srcset="${damfn.getRendition(slide.image, "slideshow-1024").getLink()}, ${damfn.getRendition(slide.image, "slideshow-1024@2x").getLink()} 2x">
+				<source media="(max-width: 1441px)"
+						srcset="${damfn.getRendition(slide.image, "slideshow-1440").getLink()}, ${damfn.getRendition(slide.image, "slideshow-1440@2x").getLink()} 2x">
+				<source media="(max-width: 1921px)"
+						srcset="${damfn.getRendition(slide.image, "slideshow-1920").getLink()}, ${damfn.getRendition(slide.image, "slideshow-1929@2x").getLink()} 2x">
+				<source
+						srcset="${damfn.getRendition(slide.image, "slideshow-2560").getLink()}, ${damfn.getRendition(slide.image, "slideshow-2560@2x").getLink()} 2x">
+
+				<img src="${damfn.getRendition(slide.image, "slideshow-1920").getLink()}" alt="">
+			</picture>
+			[/#if]
+		</div>
+	[/#list]
+	</div>
 [/#if]
 </magnolia-slideshow>
